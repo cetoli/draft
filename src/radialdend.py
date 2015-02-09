2b96e3eb71b954678a8e4f090fe52d4c3c42499d
cetoli <carlo@nce.ufrj.br>
2015-02-09 21:25:03 -0200

    feat(dend): Pinta varios niveis de ramos

--- src/radialdend.py
+++ src/radialdend.py
@@ -21,16 +21,19 @@ Impressious - Pacote cliente
 ############################################################
 Define a função main do módulo impressous, criando uma instância de Impressious
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'carlo'
-from math import pi, sin, cos
+from math import pi, sin, cos, sqrt
+OFF = 300
 CF = 2 * pi
 GIL = "produto operação conteúdo"
 FIL = "matemática linguagem ciência"
 FID = range(20)
 ONT = range(5)
 DIM = []
-DEMO = "blue "*8 + "green "*8 + "red "*8 + "yellow "*8 + "purple "*8
+MKD = 5
+DEMO = "blue "*MKD + "green "*MKD + "red "*MKD + "orange "*MKD + "purple "*MKD
+DEMO = "blue "*MKD + "green "*MKD + "red "*MKD
 DEMO = DEMO.split()
 
 
@@ -40,30 +43,46 @@ class Dendogram:
         """Inicia o dendograma"""
         self.b = browser
         self.pena = svg = browser.svg
-        title = svg.text('Title',x=70,y=25,font_size=22, text_anchor="middle")
-        circle = svg.circle(cx="70", cy="120", r="40", stroke="black", stroke_width="2", fill="red")
+        #title = svg.text('Title',x=70,y=25,font_size=22, text_anchor="middle")
+        circle = svg.circle(cx="300", cy="300", r="40", stroke="black", stroke_width="2", fill="red")
 
         div = browser.doc['pydiv']
         self.tela = panel = svg.svg(width=600, height=600)
         div <= panel
 
-        panel <= title
+        #panel <= title
         panel <= circle
-        self.drawline(40, 50, 40, 150)
-        self.drawtier()
+        #self.drawline(40, 50, 40, 150)
+        # self.drawtier()
+        self.drawdend()
 
     def drawdend(self):
-        self.drawtier(100, 200, "blue green red yellow purple".split())
+        # self.drawtier(40, 100, "blue green red orange purple".split())
+        self.drawtier(40, 100, "blue green red".split())
+        self.drawtier(100, 160)
+        self.drawtier(160, 220, "blue green red".split()*15)
+
+    def drawarc(self, x0, y0, x1, y1, color):
+        x0, y0, x1, y1 = x0+OFF, y0+OFF, x1+OFF, y1+OFF
+        cx = cy = sqrt(((x1 - x0)/2)**2 + ((y1 - y0)/2)**2)
+        self.tela <= self.pena.path(d="M %f %f A %f %f 0 0 1 %f %f" % (x0, y0, cx, cy, x1, y1),
+                                    stroke=color, stroke_width="2", fill="none")
 
     def drawtier(self, l0=100, l1=200, color=DEMO):
         # l0, l1 = 100, 200
         lines = len(color)
         step = CF / lines
+        xa, ya = sin(0)*l0, cos(0)*l0
         for r in range(lines):
             d0, h0 = sin(r*step), cos(r*step)
-            self.drawline(d0*l0, h0*l0, d0*l1, h0*l1, color[r])
+            x0, y0, x1, y1 = d0*l0, h0*l0, d0*l1, h0*l1
+            #self.drawarc(xa, ya, xa+10, ya+10, color[r])
+            self.drawarc(xa, ya, x0, y0, color[r])
+            xa, ya = x0, y0
+            self.drawline(x0, y0, x1, y1, color[r])
 
     def drawline(self, x0, y0, x1, y1, color="brown"):
+        x0, y0, x1, y1 = x0+OFF, y0+OFF, x1+OFF, y1+OFF
         line = self.pena.line(x1=x0, y1=y0, x2=x1, y2=y1,
                 stroke=color, stroke_width="2")
         self.tela <= line
---
+++ src/test/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'carlo'
+from .glow import glow
