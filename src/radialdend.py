d11b4c15089c4abf358172dc6418232dd26ded02
cetoli <carlo@ufrj.br>
2015-02-23 13:44:20 -0300

    Tentativa imperfeita de espaçamento

--- src/radialdend.py
+++ src/radialdend.py
@@ -31,9 +31,9 @@ FIL = "matemática linguagem ciência"
 FID = range(20)
 ONT = range(5)
 DIM = []
-MKD = 5
+MKD = 4
 DEMO = "blue "*MKD + "green "*MKD + "red "*MKD + "orange "*MKD + "purple "*MKD
-DEMO = "blue "*MKD + "green "*MKD + "red "*MKD
+DEMO = "blue "*MKD + "green "*MKD + "red "*6
 DEMO = DEMO.split()
 
 
@@ -43,24 +43,24 @@ class Dendogram:
         """Inicia o dendograma"""
         self.b = browser
         self.pena = svg = browser.svg
-        #title = svg.text('Title',x=70,y=25,font_size=22, text_anchor="middle")
+        # title = svg.text('Title',x=70,y=25,font_size=22, text_anchor="middle")
         circle = svg.circle(cx="300", cy="300", r="40", stroke="black", stroke_width="2", fill="red")
 
         div = browser.doc['pydiv']
         self.tela = panel = svg.svg(width=600, height=600)
         div <= panel
 
-        #panel <= title
-        panel <= circle
-        #self.drawline(40, 50, 40, 150)
+        # panel <= title
+        # self.drawline(40, 50, 40, 150)
         # self.drawtier()
         self.drawdend()
+        panel <= circle
 
     def drawdend(self):
         # self.drawtier(40, 100, "blue green red orange purple".split())
-        self.drawtier(40, 100, "blue green red".split())
-        self.drawtier(100, 160)
-        self.drawtier(160, 220, "blue green red".split()*15)
+        self.drawtier(40, 100, "blue green red".split(), [5, 5, 7], [1, 1, 1])
+        self.drawtier(100, 160, DEMO, [4]*14, [3, 3, 5])
+        self.drawtier(160, 220, "blue green red".split()*14, [3]*14, [2]*14)
 
     def drawarc(self, x0, y0, x1, y1, color):
         x0, y0, x1, y1 = x0+OFF, y0+OFF, x1+OFF, y1+OFF
@@ -68,23 +68,31 @@ class Dendogram:
         self.tela <= self.pena.path(d="M %f %f A %f %f 0 0 1 %f %f" % (x0, y0, cx, cy, x1, y1),
                                     stroke=color, stroke_width="2", fill="none")
 
-    def drawtier(self, l0=100, l1=200, color=DEMO):
+    def drawtier(self, l0=100, l1=200, color=DEMO, spacing=[], arcs=[]):
         # l0, l1 = 100, 200
         lines = len(color)
-        step = CF / lines
+        spaces = sum(spacing)  # + len(spacing)
+        step = CF / spaces  # lines
         xa, ya = sin(0)*l0, cos(0)*l0
+        doarc = arcs.pop(0)
+
         for r in range(lines):
-            d0, h0 = sin(r*step), cos(r*step)
+            angle = spacing.pop(0)
+            d0, h0 = sin(r*step*angle), cos(r*step*angle)
             x0, y0, x1, y1 = d0*l0, h0*l0, d0*l1, h0*l1
-            #self.drawarc(xa, ya, xa+10, ya+10, color[r])
-            self.drawarc(xa, ya, x0, y0, color[r])
+            # self.drawarc(xa, ya, xa+10, ya+10, color[r])
+            if doarc:
+                self.drawarc(xa, ya, x0, y0, color[r])
+                doarc -= 1
+            else:
+                doarc = arcs.pop(0)
             xa, ya = x0, y0
             self.drawline(x0, y0, x1, y1, color[r])
 
     def drawline(self, x0, y0, x1, y1, color="brown"):
         x0, y0, x1, y1 = x0+OFF, y0+OFF, x1+OFF, y1+OFF
         line = self.pena.line(x1=x0, y1=y0, x2=x1, y2=y1,
-                stroke=color, stroke_width="2")
+                              stroke=color, stroke_width="2")
         self.tela <= line
 
 
