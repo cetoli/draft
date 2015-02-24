0e6edd159c55d7ccf9809cd19bd2ea5574faaec2
cetoli <carlo@ufrj.br>
2015-02-24 16:35:46 -0300

    Funcionando, falta angulos e legendas

--- src/radialdend.py
+++ src/radialdend.py
@@ -56,11 +56,17 @@ class Dendogram:
         self.drawdend()
         panel <= circle
 
+    def drawdendo(self):
+        self.drawtier(40, 100, "blue green red".split(), [4, 4, 6], [1, 1, 1])
+        self.drawtier(100, 160, DEMO, [4]*14, [3, 3, 5], -CF/(7))
+        self.drawtier(160, 220, "blue green red".split()*14, [3]*14*3, [2]*14, -CF/(14*3))
+        self.drawtier(220, 280, "blue green red".split()*14*3, [3]*14*3*3, [2]*14*3, -CF/(14*3*3))
+
     def drawdend(self):
-        # self.drawtier(40, 100, "blue green red orange purple".split())
-        self.drawtier(40, 100, "blue green red".split(), [5, 5, 7], [1, 1, 1])
-        self.drawtier(100, 160, DEMO, [4]*14, [3, 3, 5])
-        self.drawtier(160, 220, "blue green red".split()*14, [3]*14, [2]*14)
+        self.drawtier(40, 100, "blue green red".split(), [4, 4, 4], [1, 1, 1])
+        self.drawtier(100, 160, "blue green red".split()*3, [6, 4, 4]*3, [2]*3, -CF/7)
+        self.drawtier(160, 220, DEMO*3, [3]*14*3, [3, 3, 5]*3, -CF/(7))
+        self.drawtier(220, 280, "blue green red".split()*14*3, [3]*14*3*3, [2]*14*3, -2*CF/(14*3*3))
 
     def drawarc(self, x0, y0, x1, y1, color):
         x0, y0, x1, y1 = x0+OFF, y0+OFF, x1+OFF, y1+OFF
@@ -68,24 +74,29 @@ class Dendogram:
         self.tela <= self.pena.path(d="M %f %f A %f %f 0 0 1 %f %f" % (x0, y0, cx, cy, x1, y1),
                                     stroke=color, stroke_width="2", fill="none")
 
-    def drawtier(self, l0=100, l1=200, color=DEMO, spacing=[], arcs=[]):
+    def drawtier(self, l0=100, l1=200, color=DEMO, spacing=(), arcs=(), off=0):
         # l0, l1 = 100, 200
         lines = len(color)
         spaces = sum(spacing)  # + len(spacing)
         step = CF / spaces  # lines
         xa, ya = sin(0)*l0, cos(0)*l0
-        doarc = arcs.pop(0)
+        doarc = arcs.pop(0)+1
+        angle = 0
+        first = True
 
         for r in range(lines):
-            angle = spacing.pop(0)
-            d0, h0 = sin(r*step*angle), cos(r*step*angle)
+            angle += spacing.pop(0)*step
+            t = off
+            d0, h0 = sin(angle+t), cos(angle+t)
             x0, y0, x1, y1 = d0*l0, h0*l0, d0*l1, h0*l1
-            # self.drawarc(xa, ya, xa+10, ya+10, color[r])
             if doarc:
-                self.drawarc(xa, ya, x0, y0, color[r])
+                if not first:
+                    self.drawarc(xa, ya, x0, y0, color[r])
                 doarc -= 1
             else:
                 doarc = arcs.pop(0)
+            first = False
+            # self.drawarc(xa, ya, xa+10, ya+10, color[r])
             xa, ya = x0, y0
             self.drawline(x0, y0, x1, y1, color[r])
 
