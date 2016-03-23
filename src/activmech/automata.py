32bcc4940e454b354e7f1342f92e9efd476e7245
carlo <carlo@nce.ufrj.br>
2016-03-23 17:57:09 -0300

    feat(automata): Create color table

--- src/activmech/automata.py
+++ src/activmech/automata.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF8 -*-
-# Este arquivo é parte do programa Impressious
+# Este arquivo é parte do programa Automata
 # Copyright 2014-2015 Carlo Oliveira <carlo@nce.ufrj.br>,
 # `Labase <http://labase.selfip.org/>`__; `GPL <http://is.gd/3Udt>`__.
 #
-# Impressious é um software livre; você pode redistribuí-lo e/ou
+# Automata é um software livre; você pode redistribuí-lo e/ou
 # modificá-lo dentro dos termos da Licença Pública Geral GNU como
 # publicada pela Fundação do Software Livre (FSF); na versão 2 da
 # Licença.
@@ -23,37 +23,36 @@ Cria a tabela do automata
 __author__ = 'carlo'
 GR = 204
 UP = 24
-SUBF = "M C L".split()
-SUBC = dict(M=(UP//2, 0, 0), C=(0, UP, 0), L=(0, 0, UP))
-ONTO = "S T I".split()
-FILO = "PI PM PS ME".split()
-PSUBF = "M. C. L.".split()
-OFILO = "Cajado,Diferencia Cores,Grupos;Fogo,Sinais,Conta Alimentos;Tinta,Desenho Rupestre,Desenho Rupestre;" \
-        "Palafita,Marca Habitação,Conta Animais".split(";")
-OFILO = [tuple(filo.split(",")) for filo in OFILO]
-PONTO = "_Sistema _Transformacao _Implicacao".split()
-PFILO = "P.I. P.M. P.S. ME.".split()
-TFILO = "PALEOLÍTICO INFERIOR,PALEOLÍTICO MÉDIO,PALEOLÍTICO SUPERIOR,MESOLÍTICO".split(",")
-MICRO = "ORC PAR SIP PRO INF INO EXP COP REG PSS TRA PAS".split()
-MICROT = "Objeto Real do Conhecimento,Paradoxo,Situação Problema,Prototipagem,Inferência,Inovação,Expansão Pioneira," +\
-    "Conteúdo Processo,Regras Generativas,Processos Simultâneos e Sucessivos,Transcendência,Patamares Superiores"
-NMICROT = ["%d.%s" % (n+1, t) for n, t in enumerate(MICROT.split(","))]
+SUBF = "NO CARTA AGUA CHAVE ZOOLO SEISPA".split()
+SUBC = dict(NO=(0, 0, 0), CARTA=(UP // 2, 0, 0), AGUA=(0, UP, 0), CHAVE=(0, 0, UP),
+            ZOOLO=(UP // 2, UP, 0), SEISPA=(0, UP, UP))
+FUNTEST = "Funções Executivas/ Jogos,Jogo das Cartas,Jogo da Água," \
+        "Jogo da Chave,Jogo do Zoológico,Jogo das 6 Partes".split(",")
+FUNKEYS = "Capacidade,Sequenciamento,Atenção,Resistência," \
+          "Feedback,Coordenação,Ambientação," \
+          "Habilidade".split(",")
+FUNEXEC = "Capacidade de Planejamento,Sequenciamento de Comportamento,Atenção Sustentada,Resistência a Interferência," \
+          "Uso de Feedback,Coordenação de Atividade Simultânea,Troca de Ambientação," \
+          "Habilidade de lidar com novas situações".split(",")
 PREF = "https://activufrj.nce.ufrj.br/wiki/NeuroXIV_1/"
 TBL = '<table border="1" cellpadding="0" cellspacing="0">'
+THD = "\t<thead>"
 TBD = "\t<tbody>"
 TRO = "\t\t<tr>"
-TCO = '\t\t\t<td style="background-color: rgb(%d, %d, %d);">'
+TCO = '\t\t\t<td style="width:%dpx;background-color: rgb(%d, %d, %d);">'
 FTBL = "</table>"
+FTHD = "\t</thead>"
 FTBD = "\t</tbody>"
 FTRO = "\t\t</tr>"
 FTCO = "\t\t\t</td>"
+PDATA = '\t\t\t\t<p align="center">%s</p>'
+TDATA = TCO + "\n" + PDATA + "\n" + FTCO
 LINK = '<a href="https://activufrj.nce.ufrj.br/wiki/NeuroXIV_1/%s#%s">%s</a>'
 TITULO = '<p><a id="%s" name="%s"></a>%s</p><p>&nbsp;</p>'
 HEADC = tuple(x+y for s in SUBF for x, y in zip([GR]*3, SUBC[s]))
 HEADO = [tuple(x+y+UP for x, y in zip([GR]*3, SUBC[s])) for s in SUBF]
-HEAD = """
-<tr>
-        <td colspan="10" style="width:1188px;">
+HEAD = """<tr>
+        <td colspan="20" style="width:1188px;">
         <p align="center">%s</p>
         </td>
     </tr>
@@ -75,59 +74,33 @@ HEAD = """
         <td colspan="3" style="width:269px;background-color: rgb(%d, %d, %d);">
         <p align="center">Ci&ecirc;ncia</p>
         </td>
-    </tr>
-    <tr>
-        <td colspan="3" style="width:290px;background-color: rgb(%d, %d, %d);">
-        <p align="center">Ontog&ecirc;nese - %s</p>
-        </td>
-        <td colspan="3" style="width:287px;background-color: rgb(%d, %d, %d);">
-        <p align="center">Ontog&ecirc;nese - %s</p>
-        </td>
-        <td colspan="3" style="width:269px;background-color: rgb(%d, %d, %d);">
-        <p align="center">Ontog&ecirc;nese - %s</p>
-        </td>
-    </tr>
-    <tr>
-        <td style="width:93px;">
-        <p align="center">Sistema</p>
-        </td>
-        <td style="width:104px;">
-        <p align="center">Transforma&ccedil;&atilde;o</p>
-        </td>
-        <td style="width:93px;">
-        <p align="center">Implica&ccedil;&atilde;o</p>
-        </td>
-        <td style="width:91px;">
-        <p align="center">Sistema</p>
-        </td>
-        <td style="width:104px;">
-        <p align="center">Transforma&ccedil;&atilde;o</p>
-        </td>
-        <td style="width:92px;">
-        <p align="center">Implica&ccedil;&atilde;o</p>
-        </td>
-        <td style="width:90px;">
-        <p align="center">Sistema</p>
-        </td>
-        <td style="width:90px;">
-        <p align="center">Transforma&ccedil;&atilde;o</p>
-        </td>
-        <td style="width:90px;">
-        <p align="center">Implica&ccedil;&atilde;o</p>
-        </td>
-    </tr>
-"""
+    </tr>"""
 
 
 def tabela():
     print(TBL)
     print(TBD)
-    for a, f, o in zip(FILO, TFILO, OFILO):
-        headc = tuple(x+(y,) for y, x in zip(o[::-1], HEADO))
-        head = tuple((f,)+HEADC+headc)
-        odd = False
-        print(head)
+    print(TRO)
+    for fkey, fname in zip(SUBF, FUNTEST):
+        cellw = 250 if fkey == "NO" else 150
+        cell = TDATA % ((cellw,) + tuple(GR + color for color in SUBC[fkey]) + (fname,))
+        print(cell)
         # print(HEAD % head)
+    odd = False
+    print(FTRO)
+    for funex in FUNEXEC:
+        print(TRO)
+        for fkey, fname in zip(SUBF, FUNTEST):
+            cellw = 250 if fkey == "NO" else 150
+            name = funex if fkey == "NO" else funex[:3] + fkey
+            bcolor = GR if odd else GR + 24
+            cell = TDATA % ((cellw,) + tuple(bcolor + color for color in SUBC[fkey]) + (name,))
+            print(cell)
+            # print(HEAD % head)
+        print(FTRO)
+        odd = not odd
+
+        '''
         for b, st in zip(MICRO, MICROT.split(",")):
             color = GR if odd else GR + UP
             odd = not odd
@@ -143,6 +116,7 @@ def tabela():
                     print(LINK % (link, sigla, sigla))
                     print(FTCO)
             print(FTRO)
+            '''
     print(FTBD)
     print(FTBL)
 
